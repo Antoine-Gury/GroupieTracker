e9c5e9b4dfdbb437c902170b7126fda5b6a99174
Antoine-Gury <antoinegury@outlook.fr>
2025-12-12 16:57:49 +0100

    ajout db

--- main.go
+++ main.go
@@ -6,6 +6,15 @@ import (
 )
 
 func main() {
+	// Initialiser la connexion à la DB
+	pool, err := src.InitDB()
+	if err != nil {
+		log.Fatalf("initialisation DB impossible: %v", err)
+	}
+	defer pool.Close()
+	log.Println("Connexion DB OK")
+
+	// Démarrer le serveur
 	srv, err := src.NewServer()
 	if err != nil {
 		log.Fatalf("initialisation impossible: %v", err)
